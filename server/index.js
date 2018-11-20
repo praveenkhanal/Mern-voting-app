1de5bc012a25eaa72a9ecad86d75f465ed13d312
praveenkhanal <praveen_fren@hotmail.com>
2018-11-20 13:30:53 -0800

    login/register routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const express = require ('express');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+const db = require('./models');
 const handle = require('./handlers');
+const routes = require('./routes');
+
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({hello: 'world'}));
+app.use('/api/auth', routes.auth);
 
 
 app.use(handle.notFound);
