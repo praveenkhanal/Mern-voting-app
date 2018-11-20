c27265e71604516772d3837cc2f383734819f339
praveenkhanal <praveen_fren@hotmail.com>
2018-11-19 20:40:43 -0800

    error setup

--- server/index.js
+++ server/index.js
@@ -1,21 +1,14 @@
 const express = require ('express');
 
+const handle = require('./handlers');
+
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => res.json({hello: 'world'}));
 
-app.use((req, res, next) =>{
-  const err = new Error('Not found');
-  err.status = 404;
 
-  next (err);
-});
-app.use ((err, req, res, next) =>{
-  res.status(err.status || 500).json({
-    err: err.message || 'Something went wrong'
-
-  });
-});
+app.use(handle.notFound);
+app.use(handle.errors);
 
 app.listen(port, console.log(`Server started on Port ${port}`));
